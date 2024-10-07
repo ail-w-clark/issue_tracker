07d5115f0b364692ef822952d1ee3e8d4db9e47e
ail-w-clark <160149680+ail-w-clark@users.noreply.github.com>
2024-10-06 20:06:26 -0400

    initial commit

---
+++ routes/api.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function (app) {
+
+  app.route('/api/issues/:project')
+  
+    .get(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .post(function (req, res){
+      let project = req.params.project;
+      res.json(`posting: ${project.issue_title}`);
+      
+    })
+    
+    .put(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .delete(function (req, res){
+      let project = req.params.project;
+      
+    });
+    
+};
